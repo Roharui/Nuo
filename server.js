3f559e1c7cd28f264f583eaca9848a48b42caad1
roharui <jack555586@gmail.com>
2019-11-25 16:49:40 +0900

    js static

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 
 const fs  = require('fs')
- 
+
+app.use(express.static('./static'))
 app.use(express.json())
 
 app.get('/', function(req, res){
